37ef051cf3fd6ff5696a4c7bb9db5068b1451588
Marijn Zwemmer <marijn@marijnzwemmer.com>
2018-05-26 18:02:38 +0200

    Initial commit

---
+++ Assets/AutocompleteSearchField/Scripts/Editor/Examples/AssetDatabaseSearchExample.cs
@@ -0,0 +1,53 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace AutocompleteSearchField
+{
+	public class AssetDatabaseSearchExample : EditorWindow
+	{
+		[MenuItem("Window/Autocomplete Searchbar/AssetDatabase Search Example")]
+		static void Init()
+		{
+			GetWindow<AssetDatabaseSearchExample>("AssetDatabase Example").Show();
+		}
+
+		[SerializeField]
+		AutocompleteSearchField autocompleteSearchField;
+
+		void OnEnable()
+		{
+			if (autocompleteSearchField == null) autocompleteSearchField = new AutocompleteSearchField();
+			autocompleteSearchField.onInputChanged = OnInputChanged;
+			autocompleteSearchField.onConfirm = OnConfirm;
+		}
+
+		void OnGUI ()
+		{
+			GUILayout.Label("Search AssetDatabase", EditorStyles.boldLabel);
+			autocompleteSearchField.OnGUI();
+		}
+
+		void OnInputChanged(string searchString)
+		{
+			autocompleteSearchField.ClearResults();
+			if(!string.IsNullOrEmpty(searchString) )
+			{
+				foreach (var assetGuid in AssetDatabase.FindAssets(searchString))
+				{
+					var result = AssetDatabase.GUIDToAssetPath(assetGuid);
+					if(result != autocompleteSearchField.searchString)
+					{
+						autocompleteSearchField.AddResult(result);
+					}
+				}
+			}
+		}
+
+		void OnConfirm(string result)
+		{
+			var obj = AssetDatabase.LoadMainAssetAtPath(autocompleteSearchField.searchString);
+			Selection.activeObject = obj;
+			EditorGUIUtility.PingObject(obj);
+		}
+	}
+}
---
+++ Assets/AutocompleteSearchField/Scripts/Editor/Examples/GitHubSearchExample.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace AutocompleteSearchField
+{
+	public class GitHubSearchExample : EditorWindow
+	{
+		[MenuItem("Window/Autocomplete Searchbar/GitHub Search Example")]
+		static void Init()
+		{
+			GetWindow<GitHubSearchExample>("GitHub Example").Show();
+		}
+
+		[SerializeField]
+		AutocompleteSearchField autocompleteSearchField;
+
+		WWW activeWWW;
+
+		void OnEnable()
+		{
+			if (autocompleteSearchField == null) autocompleteSearchField = new AutocompleteSearchField();
+			autocompleteSearchField.onInputChanged = OnInputChanged;
+			autocompleteSearchField.onConfirm = OnConfirm;
+			EditorApplication.update += OnUpdate;
+		}
+
+		void OnDisable()
+		{
+			EditorApplication.update -= OnUpdate;
+		}
+
+		void OnGUI ()
+		{
+			GUILayout.Label("Search GitHub", EditorStyles.boldLabel);
+			autocompleteSearchField.OnGUI();
+		}
+
+		void OnInputChanged(string searchString)
+		{
+			if(string.IsNullOrEmpty(searchString)) return;
+
+			autocompleteSearchField.ClearResults();
+			var query = string.Format("https://api.github.com/search/repositories?q={0}&sort=stars&order=desc", searchString);
+			activeWWW = new WWW(query);
+		}
+
+		void OnConfirm(string result)
+		{
+			var obj = AssetDatabase.LoadMainAssetAtPath(autocompleteSearchField.searchString);
+			Selection.activeObject = obj;
+			EditorGUIUtility.PingObject(obj);
+		}
+
+		void OnUpdate()
+		{
+			if(activeWWW != null && activeWWW.isDone)
+			{
+				if(string.IsNullOrEmpty(activeWWW.error))
+				{
+					const string url = "html_url";
+
+					autocompleteSearchField.ClearResults();
+					var text = activeWWW.text;
+
+					// Hacky json "parsing"
+					foreach (var line in text.Split('\n'))
+					{
+						var nameIndex = line.IndexOf(url, StringComparison.InvariantCulture);
+						if(nameIndex > 0)
+						{
+							var result = line.Substring(nameIndex + url.Length + 1).Split('"')[1];
+							autocompleteSearchField.AddResult(result);
+						}
+					}
+
+					Debug.Log(text.Split('\n').Length);
+				}
+				else
+				{
+					Debug.LogError("Error: " + activeWWW.error);
+				}
+				activeWWW = null;
+			}
+		}
+	}
+}
